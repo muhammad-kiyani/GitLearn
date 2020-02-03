1d74d52def91100007e45cef2bdf04812a41a4af
Muhammad-Kiyani <muhammad.kiyani@archcorp.biz>
2020-02-03 22:08:37 +0500

    1 Changes Master

--- newFile.js
+++ newFile.js
@@ -7,3 +7,13 @@
       "test": "standard && node my-tests.js"
     }
   }
+  ,
+  {
+      "name": "my-cool-package",
+      "devDependencies": {
+        "standard": "*"
+      },
+      "scripts": {
+        "test": "standard && node my-tests.js"
+      }
+    }
