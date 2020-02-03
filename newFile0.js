44b37c60e6866fefdf1e3d106a892de3415e2011
Muhammad-Kiyani <muhammad.kiyani@archcorp.biz>
2020-02-03 22:47:42 +0500

    new cahnegs master

---
+++ newFile0.js
@@ -0,0 +1,29 @@
+{
+    "name": "my-cool-package",
+    "devDependencies": {
+      "standard": "*"
+    },
+    "scripts": {
+      "test": "standard && node my-tests.js"
+    }
+  }
+  ,
+  {
+      "name": "my-cool-package",
+      "devDependencies": {
+        "standard": "*"
+      },
+      "scripts": {
+        "test": "standard && node my-tests.js"
+      }
+    }
+
+    {
+        "name": "my-cool-package",
+        "devDependencies": {
+          "standard": "*"
+        },
+        "scripts": {
+          "test": "standard && node my-tests.js"
+        }
+      }
