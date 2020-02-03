69a053d2b91744231b27ca83f20d58d72034a200
Muhammad-Kiyani <muhammad.kiyani@archcorp.biz>
2020-02-03 22:13:48 +0500

    new fearture in temp1 brnach

---
+++ newFeatureAdd_1.js
@@ -0,0 +1,19 @@
+{
+    "name": "my-cool-package",
+    "devDependencies": {
+      "standard": "*"
+    },
+    "scripts": {
+      "test": "standard && node my-tests.js"
+    }
+  }
+  ,
+  {
+      "name": "my-cool-package",
+      "devDependencies": {
+        "standard": "*"
+      },
+      "scripts": {
+        "test": "standard && node my-tests.js"
+      }
+    }
