08a2cdff3c69ce648cdea8955153f2c432985c1f
Muhammad-Kiyani <muhammad.kiyani@archcorp.biz>
2020-02-03 22:46:40 +0500

    temp 2 feature

--- newFile0.js
+++ newFile0.js
@@ -7,23 +7,3 @@
       "test": "standard && node my-tests.js"
     }
   }
-  ,
-  {
-      "name": "my-cool-package",
-      "devDependencies": {
-        "standard": "*"
-      },
-      "scripts": {
-        "test": "standard && node my-tests.js"
-      }
-    }
-
-    {
-        "name": "my-cool-package",
-        "devDependencies": {
-          "standard": "*"
-        },
-        "scripts": {
-          "test": "standard && node my-tests.js"
-        }
-      }
