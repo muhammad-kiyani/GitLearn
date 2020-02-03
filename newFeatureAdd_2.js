a531444404891d989dd60664b590d2556a9cf77f
Muhammad-Kiyani <muhammad.kiyani@archcorp.biz>
2020-02-03 22:47:22 +0500

    new changes

--- newFeatureAdd_2.js
+++ newFeatureAdd_2.js
@@ -17,3 +17,12 @@
         "test": "standard && node my-tests.js"
       }
     }
+    {
+        "name": "my-cool-package",
+        "devDependencies": {
+          "standard": "*"
+        },
+        "scripts": {
+          "test": "standard && node my-tests.js"
+        }
+      }
